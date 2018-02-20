c1d9df33e3d24567504c5ce240aeafb5f3d045a0
karingula <karingula@users.noreply.github.com>
2018-02-20 10:26:11 -0500

    add event listener

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
-import bar from './bar';
+// import bar from './bar';
 
-bar();
+// bar();
+
+
+import { secretButton, secretParagraph } from './bar';
+
+var showSecret = false;
+
+secretButton.addEventListener('click', toggleSecretState);
+updateSecretParagraph();
+
+function toggleSecretState() {
+    showSecret = !showSecret;
+    updateSecretParagraph();
+    updateSecretButton()
+}
+
+function updateSecretButton() {
+    if (showSecret) {
+        secretButton.textContent = 'Hide the Secret';
+    } else {
+        secretButton.textContent = 'Show the Secret';
+    }
+}
+
+function updateSecretParagraph() {
+    if (showSecret) {
+        secretParagraph.style.display = 'block';
+    } else {
+        secretParagraph.style.display = 'none';
+    }
+}
--- js/bar.js
+++ js/bar.js
@@ -1,3 +1,7 @@
 export default function bar(){
     console.log('Hello welcome to webpack!')
 }
+
+export var secretButton = document.querySelector('#secret-button');
+export var secretParagraph = document.querySelector('#secret-paragraph');
+
