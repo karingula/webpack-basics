8c1ebf6b1fce312a87e46739bc24b2984d811e59
karingula <karingula@users.noreply.github.com>
2018-02-21 16:21:02 -0500

    html bundles

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@
 // }
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var extractPlugin = new ExtractTextPlugin({
     filename: 'main.css'
@@ -36,7 +37,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/dist'
+        //publicPath: '/dist'
     },
     module: {
         rules : [
@@ -56,10 +57,30 @@ module.exports = {
                 use: extractPlugin.extract({
                     use: ['css-loader', 'sass-loader']
                 })
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            ouputPath: 'img/',
+                            publicPath: 'img/'
+                        }
+                    }
+                ]
             }
         ]
     },
     plugins: [
-        extractPlugin
+        extractPlugin,
+        new HtmlWebpackPlugin({
+            template: 'page.html'
+        })
     ]
 };
